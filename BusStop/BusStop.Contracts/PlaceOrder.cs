6ac0bbc7b6deb152964c400d9a83ecfc99b2b0a2
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-20 16:42:23 +0100

    Defining a message

---
+++ BusStop/BusStop.Contracts/PlaceOrder.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using System;
+
+namespace BusStop.Contracts
+{
+    public class PlaceOrder : IMessage
+    {
+        public Guid ProduictId { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+    }
+}
