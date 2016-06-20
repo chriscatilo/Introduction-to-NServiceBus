75d487d030b9f419c6051a5de2d18af7741e6624
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-20 17:44:13 +0100

    Sending a message

---
+++ BusStop/BusStop.API/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using BusStop.Contracts;
+using NServiceBus;
+using System;
+using System.Web.Http;
+
+namespace BusStop.API.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        public IBus Bus { get; set; }
+
+        public Guid Get()
+        {
+            var order = new PlaceOrder()
+            {
+                OrderId = Guid.NewGuid(),
+                ProductId = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+            };
+
+            Bus.Send(order);
+
+            return order.OrderId;
+        }
+    }
+}
