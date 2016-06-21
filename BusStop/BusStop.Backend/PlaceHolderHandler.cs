40a7e442b2a4b94c1fcca76d0ad8f6b656f666bf
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-21 15:29:09 +0100

    Setting up the backend project

---
+++ BusStop/BusStop.Backend/PlaceHolderHandler.cs
@@ -0,0 +1,14 @@
+﻿using BusStop.Contracts;
+using NServiceBus;
+using System;
+
+namespace BusStop.Backend
+{
+    public class PlaceHolderHandler : IHandleMessages<PlaceOrder>
+    {
+        public void Handle(PlaceOrder message)
+        {
+            Console.WriteLine("Order received " + message.OrderId);
+        }
+    }
+}
