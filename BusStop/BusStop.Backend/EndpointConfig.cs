82a1e9f11ff155032d7637f61da87661b7eac505
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-23 17:35:44 +0100

    Distributed Transactions

--- BusStop/BusStop.Backend/EndpointConfig.cs
+++ BusStop/BusStop.Backend/EndpointConfig.cs
@@ -1,4 +1,3 @@
-
 namespace BusStop.Backend
 {
     using NServiceBus;
--- BusStop/BusStop.Backend/PlaceHolderHandler.cs
+++ BusStop/BusStop.Backend/PlaceHolderHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BusStop.Contracts;
 using NServiceBus;
+using Raven.Client.Document;
 using System;
 
 namespace BusStop.Backend
@@ -8,7 +9,31 @@ namespace BusStop.Backend
     {
         public void Handle(PlaceOrder message)
         {
+            var store = new DocumentStore
+            {
+                Url = "http://localhost:8080",
+                DefaultDatabase = "Default"	// default database
+            };
+
+            store.Initialize();
+
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Order
+                {
+                    OrderId = message.OrderId
+                });
+
+                session.SaveChanges();
+            }
+
             Console.WriteLine("Order received " + message.OrderId);
         }
     }
+
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+    }
+
 }
