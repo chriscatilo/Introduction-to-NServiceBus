091c560f2decb46bb3dcfad1dcfc52dc7fcbf609
Christopher Catilo <chriscatilo@hotmail.com>
2016-07-02 16:32:38 +0100

    Message Mutators

---
+++ BusStop/BusStop.API/Authentication/AccessTokenMutator.cs
@@ -0,0 +1,25 @@
+﻿using NServiceBus;
+using NServiceBus.MessageMutator;
+using NServiceBus.Unicast.Messages;
+using System.Web;
+
+namespace BusStop.API.Authentication
+{
+    public class AccessTokenMutator : IMutateOutgoingTransportMessages, INeedInitialization
+    {
+        public void MutateOutgoing(LogicalMessage logicalMessage, TransportMessage transportMessage)
+        {
+            var accessTokenLiteral = "access_token";
+
+            var accessToken = HttpContext.Current.Request.Params[accessTokenLiteral];
+
+            transportMessage.Headers[accessTokenLiteral] = accessToken;
+        }
+
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.RegisterComponents(
+                components => components.ConfigureComponent<AccessTokenMutator>(DependencyLifecycle.InstancePerCall));
+        }
+    }
+}
--- BusStop/BusStop.API/Controllers/OrdersController.cs
+++ BusStop/BusStop.API/Controllers/OrdersController.cs
@@ -24,8 +24,6 @@ namespace BusStop.API.Controllers
                 CustomerId = Guid.NewGuid(),
             };
             
-            _bus.SetMessageHeader(order, "access_token", accessToken);
-            
             _bus.Send(order);
 
             return order.OrderId;
