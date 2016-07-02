ed4a560f17157d491e93eb5a1ffaa6545b428386
Christopher Catilo <chriscatilo@hotmail.com>
2016-07-02 21:41:14 +0100

    Overriding Configuration

---
+++ BusStop/BusStop.Config/ErrorQueueOverride.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+namespace BusStop.Config
+{
+    public class ErrorQueueOverride : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "myerrorQ"
+            };
+        }
+    }
+}
