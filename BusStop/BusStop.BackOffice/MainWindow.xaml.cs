183a2436e87af600d2947c15ba7556089d3f3938
Christopher Catilo <chriscatilo@hotmail.com>
2016-07-03 13:04:22 +0100

    Created BusStop.BackOffice

---
+++ BusStop/BusStop.BackOffice/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BusStop.BackOffice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
